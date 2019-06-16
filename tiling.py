17438fbd5213c44680154b6cf91033b7dde6b814
Kazu <rainbowsteam8@gmail.com>
2019-06-16 17:26:39 +0900

    Add files via upload

--- tiling.py
+++ tiling.py
@@ -2,21 +2,25 @@
 # タイリングを考えるプログラム
 
 import numpy as np
+import random
 
-tiles = np.random.randint(2, size=(4,5))
+colors = 3
+tiles = np.random.randint(colors, size=(5,5))
+counter = 0
 
+for i in range(1, tiles.shape[0]):
+    while not(tiles[i,0] != tiles[i-1,0]):
+        tiles[i,0] = random.randint(0, colors-1)
 
-for i in range(tiles.shape[0]-1):
-    for j in range(tiles.shape[1]-1):
-        if tiles[i,j] == 0:
-            tiles[i+1,j] = 1
-            tiles[i,j+1] = 1
-        else:
-            tiles[i+1,j] = 0
-            tiles[i,j+1] = 0
+for j in range(1, tiles.shape[1]):
+    while not(tiles[0,j] != tiles[0,j-1]):
+        tiles[0,j] = random.randint(0, colors-1)
 
-# 一番最後の値のみ、上のプログラムだと反映されないため追記
-if tiles[tiles.shape[0]-2,tiles.shape[1]-1] == 0:
-    tiles[tiles.shape[0]-1,tiles.shape[1]-1] = 1
-else:
-    tiles[tiles.shape[0]-1,tiles.shape[1]-1] = 0
+for i in range(1, tiles.shape[0]):
+    for j in range(1,tiles.shape[1]):
+        while not(tiles[i,j] != tiles[i-1,j] and tiles[i,j] != tiles[i,j-1]):
+            tiles[i,j] = random.randint(0, colors-1)
+            counter += 1
+
+print(tiles)
+print(counter)
